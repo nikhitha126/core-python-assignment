6c635b43fbeda0daac237720a767e02e56946743
nikhitha126 <mandlanikhitha@gmail.com>
2024-12-22 23:58:16 +0530

    Update and rename ECommerce_Cart_System.py to ecommerce_cart_system.py

--- ecommerce_cart_system.py
+++ ecommerce_cart_system.py
@@ -4,6 +4,6 @@ def calculate():
     total = sum(cart_items.values())
     if len(cart_items) > 5:
         total *= 0.9
-    return f"Total Price: {total:.2f}"
+    return f"Total Price: {total}"
 cart_items = {'Laptop':50000,'Headphones':2000,'Mouse':500,'Keyboard':1500}
 print(calculate())
